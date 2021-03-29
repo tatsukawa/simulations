f71a593568c203d4cff5e5d1484e72618899f8f1
tatsukawa <ts.tatsukawa@gmail.com>
2021-03-29 17:08:22 +0900

    feat: add kuramoto-model page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './pages/Home';
+import KuramotoModel from './pages/KuramotoModel';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>      
+        <header className="App-header">
+          <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+              <a className="navbar-item" href="/">
+                シミュレーション
+              </a>
+
+              <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </a>
+            </div>
+
+            <div id="navbarBasicExample" className="navbar-menu">
+              <div className="navbar-start">
+                <Link to="/kuramoto-model" className="navbar-item"> 蔵本モデル </Link>
+              </div>
+            </div>
+          </nav>        
+        </header>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/kuramoto-model">
+            <KuramotoModel />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
